ffdf975489bcb76c77f433238728ee488db5453f
Jackson Endsley <104740533+JEazzy123@users.noreply.github.com>
2024-10-24 22:28:22 -0700

    Added connect and disconnects tests

--- TestApp.java
+++ TestApp.java
@@ -1,13 +1,41 @@
-package DBMS_Project;
-
 public class TestApp {
 	
-	// connect to database file
-	
-	// attempt to connect to a database file (not found purpose)
-	
-	// other tests
-	
-	// disconnect from database
-
+	public static void main(String args[]) {
+		
+		// connect to database file
+		
+		DatabaseManagementSystem carDB = new DatabaseManagementSystem();
+		
+		try {
+			carDB.connect("/Users/hotpo/Desktop/cardatabase.csv");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		// showing that system is connected to database
+		System.out.println();
+		System.out.println("Database file path is: " + carDB.getDBPath());
+		System.out.println("Database is connected: " + carDB.getConnect());
+		System.out.println();
+		
+		try {
+			carDB.disconnect("/Users/hotpo/Desktop/cardatabase.csv");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		// showing that system isn't connected to database
+		System.out.println();
+		System.out.println("Database file path is: " + carDB.getDBPath());
+		System.out.println("Database is connected: " + carDB.getConnect());
+		System.out.println();
+		
+		
+		// attempt to connect to a database file (not found purpose)
+		
+		// other tests
+		
+		// disconnect from database
+		
+    }
 }
