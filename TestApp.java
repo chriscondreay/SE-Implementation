245cd65ea7cccd40ac62b7de6cedd7c8c680323e
Jackson Endsley <104740533+JEazzy123@users.noreply.github.com>
2024-10-24 20:59:01 -0700

    Create TestApp.java file

---
+++ TestApp.java
@@ -0,0 +1,13 @@
+package DBMS_Project;
+
+public class TestApp {
+	
+	// connect to database file
+	
+	// attempt to connect to a database file (not found purpose)
+	
+	// other tests
+	
+	// disconnect from database
+
+}
