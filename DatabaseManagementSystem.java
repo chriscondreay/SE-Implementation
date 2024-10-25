ffe5a6fc8e161468d09c5c2a9d8470605217bc96
Jackson Endsley <104740533+JEazzy123@users.noreply.github.com>
2024-10-24 22:30:12 -0700

    added first version of connect and disconnect functionallity

--- DatabaseManagementSystem.java
+++ DatabaseManagementSystem.java
@@ -1,18 +1,73 @@
 package DBMS_Project;
 
+import java.io.*;
+
 public class DatabaseManagementSystem implements DBMS_Interface
 {
-
-	@Override
-	public void connect(String fileName) {
-		// TODO Auto-generated method stub
-		
+	private String DBMSFilePath;
+	private boolean connected;
+	
+	public DatabaseManagementSystem(){ 
+		DBMSFilePath = "";
+		connected = false;
 	}
 
+	// getters (I don't see the need for setters in this scenario as once connected to the database, 
+	// user will not change path or "connected" status till disconnect)
+	public String getDBPath() {
+        return DBMSFilePath;
+    }
+    public boolean getConnect() {
+    	return connected;
+    }
+	
 	@Override
-	public void disconnect(String fileName) {
-		// TODO Auto-generated method stub
+	public void connect(String fileName) throws FileNotFoundException {
 		
+		File f = new File(fileName);
+		 
+        // Checking if the database file exists or not
+        if (f.exists()) {
+
+        	DBMSFilePath = fileName;
+        	connected = true;
+   
+            System.out.println("Database file exists and is connected database.");
+            
+        } 
+        // database file is not found
+        else {
+        	FileNotFoundException e = new FileNotFoundException("File not found.");
+        	throw e;
+        }
+    }
+
+	@Override
+	public void disconnect(String fileName) throws FileNotFoundException{
+		File f = new File(fileName);
+		File thisFile = new File(this.DBMSFilePath);
+		 
+        // database file is found and connected to database
+        if (f.getPath().equals(thisFile.getPath()) && ((this.connected = true))) {
+
+        	DBMSFilePath = "";
+        	connected = false;
+   
+            System.out.println("Database is disconnected.");
+            
+        } 
+        // database file is found, but is already disconnected
+        else if (f.getPath().equals(this.DBMSFilePath) && (this.connected = false)) {
+        	
+        	System.out.println("Database is found, but database is already disconnected.");
+        	
+        }
+        // file is not found
+        else {
+        FileNotFoundException e = new FileNotFoundException("File not found.");
+        throw e;
+        
+        }
 	}
 
 	@Override
