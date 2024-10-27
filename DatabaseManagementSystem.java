c7df96596e7542a4ec7607271f1b1af07a9b08cd
Jackson Endsley <104740533+JEazzy123@users.noreply.github.com>
2024-10-27 13:57:38 -0700

    file contents can now be read and added into arraylists

--- DatabaseManagementSystem.java
+++ DatabaseManagementSystem.java
@@ -2,14 +2,14 @@ package DBMS_Project;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class DatabaseManagementSystem implements DBMS_Interface
 {
 	private String DBMSFilePath;
 	private boolean connected;
+	private ArrayList<Record> contents = new ArrayList<Record>();
 	
-	public DatabaseManagementSystem(){
+	public DatabaseManagementSystem(){ 
 		DBMSFilePath = "";
 		connected = false;
 	}
@@ -29,45 +29,83 @@ public class DatabaseManagementSystem implements DBMS_Interface
 		File f = new File(fileName);
 		 
         // Checking if the database file exists or not
-        if (!f.exists()|| !f.isFile()) {
-			// database file is not found
-			throw new FileNotFoundException("Database file not found");
+        if (f.exists()) {
+
+        	DBMSFilePath = fileName;
+        	connected = true;
+        	
+        	// reading csv and adding contents into array once connected
+        	BufferedReader br = new BufferedReader(new FileReader(fileName));
+        	
+        	ArrayList<String> fieldNames = new ArrayList<String>();
+        	String line = "";
+        	int count = 0;
+			
+        	try {	
+				while((line = br.readLine()) != null) {
+					
+					// temp record object which holds field values to create a complete record arraylist which will be added into larger contents arraylist
+					Record record = new Record();
+					
+					// stores field names from first line of csv (make, model, year) (count = 0))
+					// to make it easier to add to Field(name, value) objects
+					if (count == 0) {
+						String[] names = line.split(",");
+						for (int i = 0; i < names.length; i++) {
+							fieldNames.add(names[i]);
+						}
+					// creates and adds Field objects to Record arraylist
+					} else if (count > 1) {
+						String[] values = line.split(",");
+						for (int i = 0; i < values.length; i++) {
+							Field pair = new Field(fieldNames.get(i), values[i]);
+							record.addEntry(pair);
+						}
+					}
+					
+					// checks to make sure lines 1 (make, model, year) and 2 (string, string, string) from csv are not added, but all the complete
+					// records afterwards are
+					if (count > 1) {
+						contents.add(record);
+					}
+					count++;
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+       
+            System.out.println("\nDatabase file exists and is connected database.");
+            
+        } 
+        // database file is not found
+        else {
+        	throw new FileNotFoundException("File not found.");
         }
-
-		DBMSFilePath = fileName;
-		connected = true;
-
-		System.out.println("Database file exists and is connected database.");
-
-
     }
-
+	
 	@Override
 	public void disconnect(String fileName) throws FileNotFoundException{
 		File f = new File(fileName);
 		File thisFile = new File(this.DBMSFilePath);
 		 
-        // database file is found and connected to database
-        if (f.getPath().equals(thisFile.getPath()) && ((this.connected == true))) {
-
-        	DBMSFilePath = "";
-        	connected = false;
-   
-            System.out.println("Database is disconnected.");
-            
-        } 
-        // database file is found, but is already disconnected
-        else if (f.getPath().equals(this.DBMSFilePath) && (this.connected == false)) {
-        	
-        	System.out.println("Database is found, but database is already disconnected.");
-        	
-        }
-        // file is not found
-        else {
-        FileNotFoundException e = new FileNotFoundException("File not found.");
-        throw e;
-        
-        }
+        	// database file is found and connected to database
+	        if (f.getPath().equals(thisFile.getPath()) && ((this.connected))) {
+	
+	        	DBMSFilePath = "";
+	        	connected = false;
+	   
+	            System.out.println("\nDatabase is disconnected.");      
+	        } 
+	        // database file is found, but is already disconnected
+	        else if (f.getPath().equals(this.DBMSFilePath) && (!this.connected)) {
+	        	
+	        	System.out.println("\nDatabase is found, but database is already disconnected.");
+	        }
+	        // file is not found
+	        else {
+	        	throw new FileNotFoundException("File not found.");
+	        }	
 	}
 
 	@Override
@@ -90,24 +128,8 @@ public class DatabaseManagementSystem implements DBMS_Interface
 
 	@Override
 	public Record[] select(Field search) {
-
-		// checked if the database is connected
-		if (!connected) {
-			System.out.println("Databse is not connected");
-			return new Record[0];
-		}
-
-		List<Record> matchingRecords = new ArrayList<>();
-
-		Record[] records = new Record[0];
-
-		// get the value of the field and compare it with the value
-		for (Record record : records) {
-			if (record.getFieldValue(search.getFieldName()).equals(search.getValue())) {
-				matchingRecords.add(record);
-			}
-		}
-		return matchingRecords.toArray(new Record[0]);
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
@@ -115,5 +137,12 @@ public class DatabaseManagementSystem implements DBMS_Interface
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	// prints the database records
+	public void printDBRecords() {
+		for(int i = 0; i < contents.size()-1; i++) {
+    		contents.get(i).printRecord();
+    		System.out.println();
+    		}
+	}
 }
