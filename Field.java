148510464a78325b4d568d345cf06b17bc17e0ec
Jackson Endsley <104740533+JEazzy123@users.noreply.github.com>
2024-10-27 13:58:44 -0700

    added functionality to Field class

--- Field.java
+++ Field.java
@@ -1,12 +1,33 @@
 package DBMS_Project;
 
 public class Field {
+	
+	private String name;
+	private String value;
+	
+	// constructors
+	public Field() {
+		this.name = "";
+		this.value = "";
+	}
 
-    public Object getFieldName() {
-        return null;
-    }
-
-    public Object getValue() {
-        return null;
-    }
+	public Field(String name, String value) {
+		this.name = name;
+		this.value = value;
+	}
+	
+	// getters and setters
+	public String getName() {
+		return this.name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getValue() {
+		return this.value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
 }
