59b3295ea2c40c25bb62cdc25a2465df6ff0436b
Jackson Endsley <104740533+JEazzy123@users.noreply.github.com>
2024-10-28 20:07:42 -0700

    fixed comments

--- TestApp.java
+++ TestApp.java
@@ -1,71 +1,66 @@
 package DBMS_Project;
 
-import DBMS_Project.DatabaseManagementSystem;
-
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TestApp {
-
-	public static void main(String[] args) {
-
-		// connect to database file
-
+	
+	public static void main(String args[]) {
+		
 		DatabaseManagementSystem carDB = new DatabaseManagementSystem();
-
+		
 		System.out.print("Enter the file path of your csv file: ");
 		Scanner sc = new Scanner(System.in);
 		String fileName = sc.nextLine();
-
+		
+		// testing method to connect to database file
 		try {
 			carDB.connect(fileName);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-
+		
 		// showing that system is connected to database
 		System.out.println();
 		System.out.println("Database file path is: " + carDB.getDBPath());
 		System.out.println("Database is connected: " + carDB.getConnect());
 		System.out.println();
-
-		// prints records from database to ensure file is connected and files are correctly updated in arraylist
+		
+		// prints records from database to ensure file is connected and files are correctly add to arraylist
 		carDB.printDBRecords();
-
+		
 		Record newCar = new Record();
-		newCar.addEntry(new Field("make", "Honda"));
-		newCar.addEntry(new Field("model", "Civic Type R"));
+		newCar.addEntry(new Field("make", "honda"));
+		newCar.addEntry(new Field("model", "civic type R"));
 		newCar.addEntry(new Field("year", "2019"));
 
+		// prints database records to ensure the record was inserted and the database was updated correctly
 		carDB.insert(newCar);
-
-		System.out.println("New Car added!");
 		carDB.printDBRecords();
 
-		System.out.println("Checking if database has honda");
+		System.out.println("Checking if database contains a 'honda' record.");
 		System.out.println(carDB.contains("make", "Honda"));
 
-		System.out.println("Checking if database has car that has not been added");
+		System.out.println("Checking if database contains a 'chevrolet' record.");
 		System.out.println(carDB.contains("make", "chevrolet"));
 
+		// testing to see if select method
+		Field honda = new Field("make", "honda");
+		Field lambo = new Field("make", "lambo");
+		carDB.select(honda);
+		carDB.select(lambo);
+		
+		// testing disconnect method
 		try {
 			carDB.disconnect(fileName);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-
+		
 		// showing that system isn't connected to database
 		System.out.println();
 		System.out.println("Database file path is: " + carDB.getDBPath());
 		System.out.println("Database is connected: " + carDB.getConnect());
 		System.out.println();
-
-
-		// attempt to connect to a database file (not found purpose)
-
-		// other tests
-
-		// disconnect from database
-
     }
 }
